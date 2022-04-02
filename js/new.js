e6ca4c6db05da27a3a4c264526dbd6a744a42c5e
myname <erem151999@mail.ru>
2022-04-03 00:35:24 +0300

    added news popup

--- js/new.js
+++ js/new.js
@@ -1,5 +1,6 @@
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
+    e.preventDefault();
     if (targetEl.classList.contains('t282__burger')) {
         document.querySelector('.mobile__menu').classList.add('_active');
         document.querySelector('body').classList.add('_noscroll');
@@ -8,7 +9,51 @@ document.addEventListener('click', function (e) {
         document.querySelector('.mobile__menu').classList.remove('_active');
         document.querySelector('body').classList.remove('_noscroll');
     }
+    if (targetEl.classList.contains('item-news__more') || targetEl.classList.contains('item-news__readmore')) {
+        document.querySelector('.news__grid').insertAdjacentHTML('afterend', newsPopup);
+        document.querySelector('body').classList.add('_noscroll');
+        lazyPopup();
+    }
+
+    if (targetEl.classList.contains('actions-popup__close')) {
+        document.querySelector('.news__popup').remove();
+        document.querySelector('body').classList.remove('_noscroll');
+    }
+
 })
+
+function lazyPopup() {
+    const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const lazyImage = entry.target;
+                lazyImage.style.backgroundImage = `url('${lazyImage.dataset.original}')`;
+                lazyImage.classList.add('_removegif');
+                imgObserver.unobserve(lazyImage);
+            }
+        })
+    });
+    const arr = document.querySelectorAll('div[data-original]');
+    console.log(arr);
+    arr.forEach((v) => {
+        imageObserver.observe(v);
+    })
+
+    const imageObserver2 = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const lazyImage = entry.target;
+                lazyImage.src = lazyImage.dataset.src;
+                lazyImage.classList.add('_removegif');
+                imgObserver.unobserve(lazyImage);
+            }
+        })
+    });
+    const arr2 = document.querySelectorAll('img[data-src]');
+    arr2.forEach((v) => {
+        imageObserver2.observe(v);
+    })
+}
 
 document.addEventListener('DOMContentLoaded', function (e) {
     if (document.querySelector('._scroll-header')) {
@@ -51,20 +96,20 @@ document.addEventListener('DOMContentLoaded', function (e) {
         headerObserver.observe(header);
     }
 
-    if (document.querySelector('.news__slider')) {
-        new Swiper('.news__slider-wrapper', {
-            loop: true,
-            slidesPerView: "auto",
-            spaceBetween: 30,
-            centeredSlides: true,
-            effect: 'slide',
-            slidesPerGroup: 1,
-            navigation: {
-                prevEl: '.news__slider-prev',
-                nextEl: '.news__slider-next'
-            },
-        })
-    }
+    // if (document.querySelector('.news__slider')) {
+    //     new Swiper('.news__slider-wrapper', {
+    //         loop: true,
+    //         slidesPerView: "auto",
+    //         spaceBetween: 30,
+    //         centeredSlides: true,
+    //         effect: 'slide',
+    //         slidesPerGroup: 1,
+    //         navigation: {
+    //             prevEl: '.news__slider-prev',
+    //             nextEl: '.news__slider-next'
+    //         },
+    //     })
+    // }
 
     if (document.querySelector('.partners__slider')) {
         new Swiper('.partners__slider', {
@@ -125,6 +170,137 @@ document.addEventListener('DOMContentLoaded', function (e) {
         }, 800);
     }
 
-    
+
 
 })
+
+
+let newsPopup =
+    `
+<div class="news__popup">
+<div class="news__popup-actions actions-popup">
+    <div class="actions-popup__body">
+        <a href="" class="actions-popup__close">
+            <svg width="11" height="20" viewBox="0 0 11 20" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 1L10 10L1 19" stroke="#002234" stroke-width="2"></path>
+            </svg>
+        </a>
+        <div class="actions-popup__info">Федерация бокса СПб | Новости 2022-го года</div>
+        <div class="actions-popup__share">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.92 15.79" style="fill:#002234">
+                <g data-name="Слой 2">
+                    <path d="M16 9.67v6H.19v-12h7.18a7.1 7.1 0 00-1.46.89 7 7 0 00-1.08 1.11H2.19v8H14v-1.9z">
+                    </path>
+                    <path
+                        d="M17.73 5.7L12.16.13V3.8c-1.45.06-7 .73-7.62 7.08a.07.07 0 00.13 0c.49-1.35 2.11-3.43 7.49-3.52v3.88z">
+                    </path>
+                </g>
+            </svg>
+
+        </div>
+    </div>
+</div>
+<div class="news__popup-wrapper">
+    <div class="news__popup-container">
+        <div class="news__popup-top">
+            <div class="news__popup-image">
+                <img  data-src="img/2FBA4361-146F-4203-9.jpeg" src="img/1x1.png" alt="">
+            </div>
+            <div class="news__popup-content">
+                <div class="news__popup-data"> 30.03.2022 </div>
+                <h1 class="news__popup-title"> Седьмой бой Павла Сосулина на профессиональном ринге </h1>
+                <p class="news__popup-text">
+                    Спустя долгое время профессиональный бокс возвращается на Первый канал и уже 2 апреля на ринге
+                    «Крыльев Советов» в Москве пройдёт вечер профессионального бокса, где в соглавном бою примет
+                    участие петербургский спортсмен Павел Сосулин (6-0-0).
+
+                    Его соперником станет Игорь Иванченко (5-0-0). Оба боксёра ещё не знают поражений на проф-ринге,
+                    поэтому для каждого из них это серьезная проверка.
+
+                    Также в этот же вечер свой первый поединок среди профессионалов проведёт петербургский спортсмен
+                    Никита Кузьмин. Его оппонентом станет еще один дебютант – представитель Узбекистана Асилбек
+                    Мансуров.
+
+                    В главном бою сойдутся Олимпийский чемпион игр в Токио Альберт Батыргазиев (5-0-0) и Хейбулла
+                    Хаджалиев (8-2-0) из Азербайджана.
+                </p>
+            </div>
+        </div>
+        <div class="news__popup-other">Смотрите также</div>
+        <div class="news__popup-bottom">
+            <div class="news__grid-item item-news">
+                <a href="" class="item-news__link">
+                    <div class="item-news__wrapper">
+                        <div class="item-news__image">
+                            <div class="item-news__image-img" data-original="img/17EE28B7-28B7-4C1D-9.webp"></div>
+                        </div>
+                        <div class="item-news__content">
+                            <div class="item-news__tags">
+                                <span class="item-news__tag">Матчи</span>
+                                <span class="item-news__tag">Федерация</span>
+                            </div>
+                            <div class="item-news__title"> Алексей Мазур, Павел Сосулин, Илья Попов и Руслан
+                                Колесников совсем скоро выйдут на профессиональный ринг!</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="news__grid-item item-news">
+                <a href="" class="item-news__link">
+                    <div class="item-news__wrapper">
+                        <div class="item-news__image">
+                            <div class="item-news__image-img" data-original="img/17EE28B7-28B7-4C1D-9.webp"></div>
+                        </div>
+                        <div class="item-news__content">
+                            <div class="item-news__tags">
+                                <span class="item-news__tag">Матчи</span>
+                                <span class="item-news__tag">Федерация</span>
+                            </div>
+                            <div class="item-news__title"> Алексей Мазур, Павел Сосулин, Илья Попов и Руслан
+                                Колесников совсем скоро выйдут на профессиональный ринг!</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="news__grid-item item-news">
+                <a href="" class="item-news__link">
+                    <div class="item-news__wrapper">
+                        <div class="item-news__image">
+                            <div class="item-news__image-img" data-original="img/17EE28B7-28B7-4C1D-9.webp"></div>
+                        </div>
+                        <div class="item-news__content">
+                            <div class="item-news__tags">
+                                <span class="item-news__tag">Матчи</span>
+                                <span class="item-news__tag">Федерация</span>
+                            </div>
+                            <div class="item-news__title"> Алексей Мазур, Павел Сосулин, Илья Попов и Руслан
+                                Колесников совсем скоро выйдут на профессиональный ринг!</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="news__grid-item item-news">
+                <a href="" class="item-news__link">
+                    <div class="item-news__wrapper">
+                        <div class="item-news__image">
+                            <div class="item-news__image-img" data-original="img/17EE28B7-28B7-4C1D-9.webp"></div>
+                        </div>
+                        <div class="item-news__content">
+                            <div class="item-news__tags">
+                                <span class="item-news__tag">Матчи</span>
+                                <span class="item-news__tag">Федерация</span>
+                            </div>
+                            <div class="item-news__title"> Алексей Мазур, Павел Сосулин, Илья Попов и Руслан
+                                Колесников совсем скоро выйдут на профессиональный ринг!</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</div>
+
+`;
